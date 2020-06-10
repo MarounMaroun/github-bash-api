2582e7b6c5c66042a00ea7f3bee266d9d726a762
MarounMaroun <maroun.bassam@gmail.com>
2020-06-10 21:56:30 +0300

    add POST with payload to base_api

--- base_api.sh
+++ base_api.sh
@@ -3,9 +3,17 @@
 GITHUB_API_HEADER="Accept: application/vnd.github.v3+json"
 GITHUB_API_URI="https://api.github.com"
 
-base_api::call() {
+base_api::get() {
   local -r token="$1"
   local -r api="$2"
   body=$(curl -sSL -H "Authorization: token $token" -H "$GITHUB_API_HEADER" "$GITHUB_API_URI/$api")
+  echo "$body"
+}
+
+base_api::post() {
+  local -r token="$1"
+  local -r api="$2"
+  local -r data="$3"
+  body=$(curl -XPOST -sSL -H "Authorization: token $token" -H "$GITHUB_API_HEADER" "$GITHUB_API_URI/$api" --data "$data")
   echo "$body"
 }
