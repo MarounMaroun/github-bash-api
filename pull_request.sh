c697f7eee3443079298fc086ede97cf2d3d1480a
MarounMaroun <maroun.bassam@gmail.com>
2020-06-10 21:58:04 +0300

    add comment on PR API and rename base_api GET function

--- pull_request.sh
+++ pull_request.sh
@@ -9,45 +9,51 @@ TOKEN="$1"
 OWNER="$2"
 GITHUB_REPO="$3"
 PR_NUM="$4"
+BODY="$5"
 
 function list_prs() {
-  base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls"
+  base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls"
 }
 
 function get_pr() {
-  base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM"
+  base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM"
 }
 
 function list_commits() {
-  base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM/commits"
+  base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM/commits"
 }
 
 function list_commits_message_only() {
-  body=$(base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM/commits")
+  body=$(base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM/commits")
   echo "$body" | jq -r '.[] | .commit.message'
 }
 
 function list_commits_files() {
-  body=$(base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM/files")
+  body=$(base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM/files")
   echo "$body"
 }
 
 function list_commits_files_name_only() {
-  body=$(base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM/files")
+  body=$(base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM/files")
   echo "$body" | jq -r '.[] | .filename'
 }
 
 function prs_count() {
-  body=$(base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls")
+  body=$(base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls")
   echo "$body" | jq '. | length'
 }
 
 function get_pr_author() {
-  body=$(base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM")
+  body=$(base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM")
   echo "$body" | jq '.user.login'
 }
 
 function get_pr_changed_files_count() {
-  body=$(base_api::call "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM")
+  body=$(base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/pulls/$PR_NUM")
   echo "$body" | jq '.changed_files'
+}
+
+function comment_on_pr() {
+  res=$(base_api::post "$TOKEN" "repos/$OWNER/$GITHUB_REPO/issues/$PR_NUM/comments" "{\"body\": \"$BODY\"}")
+  echo "$res" | jq '.body'
 }
