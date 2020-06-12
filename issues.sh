5480b9345e8735ab9aebbcbec1eebe0ad69543d2
MarounMaroun <maroun.bassam@gmail.com>
2020-06-12 17:20:43 +0300

    add create/delete label on issues

--- issues.sh
+++ issues.sh
@@ -8,6 +8,8 @@ utils::validate_jq
 TOKEN="$1"
 OWNER="$2"
 GITHUB_REPO="$3"
+ISSUE_NUM="$4"
+PAYLOAD="$5"
 
 function list_issues() {
   base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/issues"
@@ -21,6 +23,16 @@ function count_issues() {
 function get_unique_assignee() {
   body=$(base_api::get "$TOKEN" "repos/$OWNER/$GITHUB_REPO/issues")
   echo "$body" | jq -r '.[] | .assignee.login' | uniq -u
+}
+
+function add_issue_label() {
+  # payload should be: {"labels": ["label2", "label2"]}
+  base_api::post "$TOKEN" "repos/$OWNER/$GITHUB_REPO/issues/$ISSUE_NUM/labels" "$PAYLOAD"
+}
+
+function remove_issue_label() {
+  # payload should be the name of the label
+  base_api::delete "$TOKEN" "repos/$OWNER/$GITHUB_REPO/issues/$ISSUE_NUM/labels/$PAYLOAD"
 }
 
 function get_all_desciptions() {
