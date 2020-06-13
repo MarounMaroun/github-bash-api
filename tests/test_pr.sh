eab196bc50a2090b036f405aeffc45e534838757
MarounMaroun <maroun.bassam@gmail.com>
2020-06-13 19:50:22 +0300

    more verbose test info

--- tests/test_pr.sh
+++ tests/test_pr.sh
@@ -10,5 +10,6 @@ TOKEN="$1"
 OWNER="$2"
 GITHUB_REPO="$3"
 
+echo "---= Running PR count test =---"
 count=$(prs_count "$TOKEN" "$OWNER" "$GITHUB_REPO")
-[[ "$count" -eq 2 ]] || { echo "PR count test failed"; exit 1; }
+[[ "$count" -eq 2 ]] || { echo "[X] PR count test failed"; exit 1; }
