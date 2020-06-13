bd98e826280bc9e92cd78e4e7b162996e6b90f5c
MarounMaroun <maroun.bassam@gmail.com>
2020-06-13 20:17:45 +0300

    more verbose info

--- tests/test_pr.sh
+++ tests/test_pr.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # test script should be ran from the project's WD
 source "pull_request.sh"
 
@@ -10,5 +12,7 @@ TOKEN="$1"
 OWNER="$2"
 GITHUB_REPO="$3"
 
+echo "---= Running PR count test =---"
 count=$(prs_count "$TOKEN" "$OWNER" "$GITHUB_REPO")
-[[ "$count" -eq 2 ]] || { echo "PR count test failed"; exit 1; }
+echo "[INFO] got PRs count of $count"
+[[ "$count" -eq 2 ]] || { echo "[X] PR count test failed"; exit 1; }
